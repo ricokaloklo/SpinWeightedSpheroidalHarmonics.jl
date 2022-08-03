ff91d843ee75a2b29efdbb82cc287a07e5b90c17
Rico Lo <ka-lok.lo@ligo.org>
2022-08-03 15:11:44 -0700

    Use keyword argument for optional input

--- src/SpinWeightedSpheroidalHarmonics.jl
+++ src/SpinWeightedSpheroidalHarmonics.jl
@@ -21,7 +21,7 @@ struct SpectralDecompositionCoefficients
     SpectralDecompositionCoefficients(input) = spectral_coefficients(input.c, input.s, input.l, input.m, input.N)
 end
 
-function _unnormalized_spin_weighted_spheroidal_harmonic(coefficients_params, coefficients, theta, phi, theta_derivative::Int=0, phi_derivative::Int=0)
+function _unnormalized_spin_weighted_spheroidal_harmonic(coefficients_params, coefficients, theta, phi; theta_derivative::Int=0, phi_derivative::Int=0)
     # Compute the spin-weighted spherical harmonics needed
     output = 0.0
     spherical_harmonics_l = construct_all_l_in_matrix(coefficients_params.s, coefficients_params.m, coefficients_params.N)
@@ -41,7 +41,7 @@ function _compute_normalization_constant(coefficients_params, coefficients)
     where nf is the normalization constant needed
     =#
     
-    integrand(theta) = 2*pi * _unnormalized_spin_weighted_spheroidal_harmonic(coefficients_params, coefficients, theta, 0.0, 0, 0)^2 * sin(theta)
+    integrand(theta) = 2*pi * _unnormalized_spin_weighted_spheroidal_harmonic(coefficients_params, coefficients, theta, 0.0)^2 * sin(theta)
     norm_sq = quadgk(theta -> integrand(theta), 0, pi)[1]
     return sqrt(norm_sq)
 end
@@ -50,7 +50,7 @@ _cached_coefficients_params = SpectralDecompositionInputParams(-2, 2, 2, 0.5+0.1
 _cached_coefficients = SpectralDecompositionCoefficients(_cached_coefficients_params)
 _cached_normalization = _compute_normalization_constant(_cached_coefficients_params, _cached_coefficients)
 
-function spin_weighted_spheroidal_harmonic(s::Int, l::Int, m::Int, c, theta, phi, theta_derivative::Int=0, phi_derivative::Int=0, N::Int=10)
+function spin_weighted_spheroidal_harmonic(s::Int, l::Int, m::Int, c, theta, phi; theta_derivative::Int=0, phi_derivative::Int=0, N::Int=10)
     global _cached_coefficients_params
     global _cached_coefficients
     global _cached_normalization
@@ -63,7 +63,7 @@ function spin_weighted_spheroidal_harmonic(s::Int, l::Int, m::Int, c, theta, phi
         _cached_normalization = _compute_normalization_constant(_cached_coefficients_params, _cached_coefficients)
     end
 
-    unnormalized_harmonic = _unnormalized_spin_weighted_spheroidal_harmonic(_cached_coefficients_params, _cached_coefficients, theta, phi, theta_derivative, phi_derivative)
+    unnormalized_harmonic = _unnormalized_spin_weighted_spheroidal_harmonic(_cached_coefficients_params, _cached_coefficients, theta, phi; theta_derivative=theta_derivative, phi_derivative=phi_derivative)
     unnormalized_harmonic/_cached_normalization
 end
 
